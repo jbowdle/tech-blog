47fca88ec8e15b922e908c48ecaa19c3c24dede9
jbowdle <johnwbowdle@gmail.com>
2023-05-11 12:14:51 -0500

    home routes now semi functional (output json, but aren't connected to views)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,59 @@
 const router = require('express').Router();
-const { Post, Comment } = require('../models');
+const { Post, Comment, User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: Comment,
-          attributes: ['content'],
-        },
-      ],
-    });
+    const postData = await Post.findAll();
 
     const posts = postData.map((post) =>
       post.get({ plain: true })
     );
 
-    res.render('homepage', {
-      p2121osts,
-    });
+    // debug
+    res.status(200).json(posts);
+
+    // code to be used once views are created:
+    // res.render('homepage', {
+    //   posts,
+    // });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'content',
+            'createdAt',
+          ],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    // debug
+    res.status(200).json(post);
+
+    // code to be used once views are created goes here:
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
